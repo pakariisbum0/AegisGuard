c74affca760298968214fbaca1788bd27529e5c6
pakariisbum0 <moonlightwhisper7@gmail.com>
2025-02-07 10:45:06 +0000

    feat: add Sepolia network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+// Replace file reading with environment variable
+const privateKey = process.env.WALLET_PRIVATE_KEY_FLOW;
 
 /** @type import('hardhat/config').HardhatUserConfig */
+// npx hardhat ignition deploy ./ignition/modules/TherapyConsent.js --network baseSepolia
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+      chainId: 4202,
+    },
+    Flow: {
+      url: "https://testnet.evm.nodes.onflow.org",
+      accounts: [privateKey],
+      gasPrice: 1000000000,
+    },
+  },
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  allowUnlimitedContractSize: true,
+  throwOnTransactionFailures: true,
+  throwOnCallFailures: true,
+  loggingEnabled: true,
 };
